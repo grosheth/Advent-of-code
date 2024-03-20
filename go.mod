9e75b0508fde8738ff3b65726757d4fe46ef858c
salledelavage <salledelavager@gmail.com>
2024-03-20 18:34:40 -0400

    testing fucking modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/grosheth/Advent-of-code
+
+go 1.21.7
--- golang/2023/day1/main.go
+++ golang/2023/day1/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/alexchao26/advent-of-code-go/util"
+	"github.com/grosheth/Advent-of-code"
 	"log"
 	"os"
 )
@@ -15,21 +15,4 @@ func main() {
 	}
 	fmt.Println(lines[0])
 
-}
-
-// read line by line into memory
-// all file contents is stores in lines[]
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
 }

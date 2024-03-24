0c891f79b7eb2e41d618efea2e01229f18770853
salledelavage <salledelavager@gmail.com>
2024-03-24 09:42:34 -0400

    test

--- golang/2023/day1/main.go
+++ golang/2023/day1/main.go
@@ -1,57 +1,68 @@
 package main
 
 import (
-	// "bufio"
 	"fmt"
 	"github.com/grosheth/Advent-of-code/golang/util"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	answer := part1(util.ReadFile("input.txt"))
+	// answer1 := part1(util.ReadFile("input.txt"))
+	// fmt.Println("Part1:", answer1)
 
-	fmt.Println("Reponse:", answer)
+	answer2 := part2(util.ReadFile("sample.txt"))
+	fmt.Println("Part2:", answer2)
 
 }
 
-func part1(file []string) []string {
-	for _, each_ln := range file {
-		var temp_list []int 
-    for char := range each_ln {
-  		if value, err := strconv.Atoi(string(char); err == nil {
-        fmt.Println(char)
-      }
-    }
-
-	}
-
-	return file
-}
-
+// func part1(file []string) int {
+// 	var numbers_list []string
+// 	answer := 0
 //
-// func part1(file string) []int {
-// 	// first digit + last digit = 2 digit number
-// 	// if one digit, double it
-// 	// sum up all these values
-// 	var numbers []int
-// 	var digits []int
-// 	for x := range file {
-// 		// Validating if its the end of line
-// 		if string(file[x]) != "" {
-//
-// 			// Validating if value is a number and adding FIRST and LAST digits to list
-// 			if value, err := strconv.Atoi(string(file[x])); err == nil {
-// 				fmt.Println(value)
-// 				if len(digits) == 0 {
-// 					digits = append(digits, value)
-// 					digits = append(digits, value)
-// 				} else {
-// 					digits[1] = value
-// 				}
+// 	for _, each_ln := range file {
+// 		var digits []int
+// 		for _, char := range each_ln {
+// 			if value, err := strconv.Atoi(string(char)); err == nil {
+// 				digits = append(digits, value)
 // 			}
-// 			numbers = digits
 // 		}
+// 		numbers := fmt.Sprint(digits[0]) + fmt.Sprint(digits[len(digits)-1])
+// 		numbers_list = append(numbers_list, fmt.Sprint(numbers))
 // 	}
-//
-// 	return numbers
+// 	for _, value := range numbers_list {
+// 		num, err := strconv.Atoi(value)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		answer += num
+// 	}
+// 	return answer
 // }
+
+func part2(file []string) int {
+	var numbers_list []string
+	answer := 0
+
+	for _, each_ln := range file {
+		var digits []int
+		var replacer = strings.NewReplacer("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9")
+		each_ln = replacer.Replace(each_ln)
+
+		for _, char := range each_ln {
+			if value, err := strconv.Atoi(string(char)); err == nil {
+				digits = append(digits, value)
+			}
+		}
+		numbers := fmt.Sprint(digits[0]) + fmt.Sprint(digits[len(digits)-1])
+		numbers_list = append(numbers_list, fmt.Sprint(numbers))
+	}
+	for _, value := range numbers_list {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println(err)
+		}
+		answer += num
+	}
+	return answer
+}

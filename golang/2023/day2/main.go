9256a141d3c9c77967362e9596380e71c7e97527
salledelavage <salledelavager@gmail.com>
2024-03-27 19:51:57 -0400

    test regexp

--- golang/2023/day2/main.go
+++ golang/2023/day2/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/grosheth/Advent-of-code/golang/util"
+	"regexp"
+	// "strings"
 )
 
 func main() {
@@ -15,10 +17,21 @@ func main() {
 }
 
 func part1(file []string) []string {
-	// answer := 0
+	answer := 0
+	// maximum number cannot be obtained for one color
+	// only 12 red cubes, 13 green cubes, and 14 blue cubes
+	green := 13
+	red := 12
+	blue := 14
+	fmt.Println(answer, green, red, blue)
 	// Split input by games
-	fmt.Println(file)
-	// maximum number cannot be shown for one color
+	for _, game := range file {
+		fmt.Println(game)
+		gameSplit, err := regexp.MatchString(game, "[0-9]")
+		println(err)
+		fmt.Println(gameSplit)
+	}
+
 	// only 12 red cubes, 13 green cubes, and 14 blue cubes
 	// count number of game that are possible
 	// Sum the ID's of each possible game

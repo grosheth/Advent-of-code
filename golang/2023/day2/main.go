5246eb07fdf594c5225b34709842713c1d295742
salledelavage <salledelavager@gmail.com>
2024-03-31 19:10:30 -0400

    Day 2 part 1

--- golang/2023/day2/main.go
+++ golang/2023/day2/main.go
@@ -4,85 +4,134 @@ import (
 	"fmt"
 	"github.com/grosheth/Advent-of-code/golang/util"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	answer1 := part1(util.ReadFile("sample.txt"))
-	fmt.Println("Part1:", answer1)
+	// answer1 := part1(util.ReadFile("input.txt"))
+	// fmt.Println("Part1:", answer1)
 
-	// answer2 := part2(util.ReadFile("input.txt"))
-	// fmt.Println("Part2:", answer2)
+	answer2 := part2(util.ReadFile("sample.txt"))
+	fmt.Println("Part2:", answer2)
 
 }
 
-func part1(file []string) []string {
+// PART 2
+// Get the biggest number of each color
+// The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
+// The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively.
+// Adding up these five powers produces the sum 2286
+func part2(file []string) int {
+	valid := true
 	answer := 0
-	// maximum number cannot be obtained for one color
-	// only 12 red cubes, 13 green cubes, and 14 blue cubes
-	green := 13
-	red := 12
-	blue := 14
+	gameId := 0
 
-	fmt.Println(answer, green, red, blue)
-	// Split input by games
-	for _, game := range file {
-		// fmt.Println(game)
-
-		// get Game ID
-		// gameid, _ := regexp.compile("[0-9]+")
-		// gamesplit := gameid.findstringsubmatch(string(game))
-		// fmt.Println("gameId", gameSplit)
-
-		// Get colors
+	for index, game := range file {
+		gameId = index + 1
 		rgx := regexp.MustCompile(`:+`)
 		gameSplit := rgx.Split(game, -1)
-		fmt.Println("gameSplit", gameSplit)
+		// fmt.Println("gameSplit", gameSplit)
 
 		for _, x := range gameSplit {
-
 			rgx := regexp.MustCompile(`;+`)
 			colorSplit := rgx.Split(x, -1)
-			fmt.Println("colorSplit", colorSplit)
+			// fmt.Println("colorSplit", colorSplit)
 
 			for _, y := range colorSplit {
-
 				rgx := regexp.MustCompile(`,+`)
 				resultSplit := rgx.Split(y, -1)
-				fmt.Println("resultSplit", resultSplit)
+				// fmt.Println("resultSplit", resultSplit)
 
 				for _, w := range resultSplit {
+					colorList := []string{"green", "red", "blue", "Game"}
+					rgx := regexp.MustCompile(`[0-9]+`)
+					number := rgx.FindStringSubmatch(w)
+					nombre, _ := strconv.Atoi(number[0])
 
-					colorList := []string{"green", "red", "blue"}
 					for _, color := range colorList {
 						if strings.Contains(w, color) {
 							switch check := color; check {
 							case "green":
-								rgx := regexp.MustCompile(`[0-9]`)
-								number := rgx.Split(y, -1)
-								fmt.Println(number)
+								if nombre > 13 {
+									gameId = 0
+								}
 							case "blue":
-								rgx := regexp.MustCompile(`,+`)
-								number := rgx.Split(y, -1)
-								fmt.Println(number)
+								if nombre > 14 {
+									gameId = 0
+								}
 							case "red":
-								rgx := regexp.MustCompile(`,+`)
-								number := rgx.Split(y, -1)
-								fmt.Println(number)
+								if nombre > 12 {
+									gameId = 0
+								}
 							}
 						}
 					}
 				}
-				// only 12 red cubes, 13 green cubes, and 14 blue cubes
-				// count number of game that are possible
-				// Sum the ID's of each possible game resultSplit[0]
-
 			}
 		}
+		// Sum of Power
+		if valid == true {
+			// fmt.Println("Adding", gameId, "to", answer)
+			answer += gameId
+		}
+		valid = true
 	}
-	return file
+	return answer
 }
 
-// func part2() {
-//
-// }
+func part1(file []string) int {
+	valid := true
+	answer := 0
+	gameId := 0
+
+	for index, game := range file {
+		gameId = index + 1
+		rgx := regexp.MustCompile(`:+`)
+		gameSplit := rgx.Split(game, -1)
+
+		for _, x := range gameSplit {
+			rgx := regexp.MustCompile(`;+`)
+			colorSplit := rgx.Split(x, -1)
+
+			for _, y := range colorSplit {
+				rgx := regexp.MustCompile(`,+`)
+				resultSplit := rgx.Split(y, -1)
+
+				for _, w := range resultSplit {
+					colorList := []string{"green", "red", "blue", "Game"}
+					rgx := regexp.MustCompile(`[0-9]+`)
+					number := rgx.FindStringSubmatch(w)
+					nombre, _ := strconv.Atoi(number[0])
+
+					for _, color := range colorList {
+						if strings.Contains(w, color) {
+							switch check := color; check {
+							case "green":
+								if nombre > 13 {
+									fmt.Println("game", gameId, "is not valid")
+									gameId = 0
+								}
+							case "blue":
+								if nombre > 14 {
+									fmt.Println("game", gameId, "is not valid")
+									gameId = 0
+								}
+							case "red":
+								if nombre > 12 {
+									fmt.Println("game", gameId, "is not valid")
+									gameId = 0
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if valid == true {
+			answer += gameId
+		}
+		valid = true
+	}
+	return answer
+}

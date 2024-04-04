6d26f870f7c107579d3d71312e1f13e16f7f6ede
salledelavage <salledelavager@gmail.com>
2024-04-03 20:28:45 -0400

    save inputs

--- golang/2023/day3/main.go
+++ golang/2023/day3/main.go
@@ -3,27 +3,56 @@ package main
 import (
 	"fmt"
 	"github.com/grosheth/Advent-of-code/golang/util"
+	"log"
+	"regexp"
 )
 
 func main() {
-	answer1 := part1(util.ReadFile("input.txt"))
+	answer1 := part1(util.ReadFile("sample.txt"))
 	fmt.Println("Part1:", answer1)
 
-	answer2 := part2(util.ReadFile("input.txt"))
-	fmt.Println("Part2:", answer2)
+	// answer2 := part2(util.ReadFile("input.txt"))
+	// fmt.Println("Part2:", answer2)
 
 }
 
+// Part 1
+// Add the part numbers
+// part numbers are adjacent to symbols
 func part1(file []string) int {
 	answer := 0
-	fmt.Println(answer)
 
+	// find symbols
+	re, err := regexp.Compile(`[-!$%^&*()#_+|~={}\[\]:";'<@>?,\/]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, x := range file {
+		x = re.ReplaceAllString(x, " ")
+		fmt.Println(x)
+	}
 	return answer
 }
 
-func part2(file []string) int {
-	answer := 0
-	fmt.Println(answer)
+// Part 2
+// func part2(file []string) int {
+// 	answer := 0
+//
+// 	return answer
+// }
 
-	return answer
-}
+// remove all symbols
+// func part1(file []string) int {
+// 	answer := 0
+//
+// 	re, err := regexp.Compile(`[^\w]`)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	for _, x := range file {
+// 		x = re.ReplaceAllString(x, "")
+// 		fmt.Println(x)
+// 	}
+// 	return answer
+// }

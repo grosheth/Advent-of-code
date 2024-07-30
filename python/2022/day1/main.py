d52b26f6296ba0f7c374e031923beaa9e92bdb4f
salledelavage <salledelavager@gmail.com>
2024-07-30 07:34:00 -0400

    adding file + cleaning

--- python/2022/day1/main.py
+++ python/2022/day1/main.py
@@ -8,7 +8,12 @@ with open("input.txt", 'r') as f:
             sum += int(i)
         else:
             for x in new_sum:
-                if sum > x: smallest = min(new_sum);new_sum.remove(smallest);new_sum.append(sum);print(new_sum);break
+                if sum > x:
+                    smallest = min(new_sum) 
+                    new_sum.remove(smallest)
+                    new_sum.append(sum)
+                    print(new_sum)
+                    break
             sum = 0
 
 sum = new_sum[0] + new_sum[1] + new_sum[2]
